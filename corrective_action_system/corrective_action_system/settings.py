a46e1b8783cf775cbc0e36b0ee8c9b82eda1ac25
bailonjr <rommelbailon00@gmail.com>
2024-10-11 23:14:59 +0800

    Added Project Modal to Add on the Table of Monitoring Log

--- corrective_action_system/corrective_action_system/settings.py
+++ corrective_action_system/corrective_action_system/settings.py
@@ -75,9 +75,9 @@ DATABASES = {
 
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR, 'static'),
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
